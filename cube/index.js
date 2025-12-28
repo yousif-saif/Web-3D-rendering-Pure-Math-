43410ce5829dceb7fb61467fcc3c93be5655df9e
yousif-saif <mrdento82@gmail.com>
2025-12-28 21:13:10 +0300

    added Cube

---
+++ cube/index.js
@@ -0,0 +1,364 @@
+game.width = 600
+game.height = 600
+ctx = game.getContext("2d")
+w = game.width
+h = game.height
+
+const COLOR = "green"
+const BACKGROUND = "black"
+const SIZE = 5
+
+
+function clear() {
+    ctx.fillStyle = BACKGROUND
+    ctx.fillRect(0, 0, w, h)
+}
+
+
+function point({x, y}) {
+    ctx.fillStyle = COLOR
+    const halfSize = SIZE/2
+    ctx.fillRect(x-halfSize, y-halfSize, SIZE, SIZE)
+
+    return {x, y}
+}
+
+
+function line(p1, p2) {
+    ctx.lineWidth = 3
+    ctx.strokeStyle = COLOR
+    ctx.beginPath()
+    ctx.moveTo(p1.x, p1.y)
+    ctx.lineTo(p2.x, p2.y)
+    ctx.stroke()
+
+}
+
+
+function calcDist2D({x1, y1}, {x2, y2}) {
+    return Math.sqrt(
+        Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2)
+    )
+
+}
+
+
+function colorSide(p1, p2) {
+    ctx.fillStyle = COLOR
+    fillSize = calcDist2D({x1: p1.x, y1: p1.y}, {x2: p2.x, y2: p2.y})
+    ctx.fillRect(p1.x, p1.y, fillSize, fillSize)
+}
+
+function screen({x, y}) {
+    // -1..1  => 0..2 => 0..1 => 0..w 
+    // -1..1 => 0..2 => 0..1 => 0..h
+
+    x = (x + 1) / 2 * w
+    y = (y*-1 + 1) / 2  * h // changed
+
+    return {x, y}
+}
+
+function webScreen({x, y}) {
+    // 0..w => 0..1 => 0..2 => -1..1
+    // 0..h => 0..1 => 
+
+    x = (x / w * 2 - 1)
+    y = (y / h * 2 - 1)*-1
+
+    return {x: +x.toFixed(2), y: +y.toFixed(2)}
+    
+}
+
+
+function project({x, y, z}) {
+    return {
+        x: x / z,
+        y: y / z
+    }
+}
+
+clear()
+
+
+// function calcDist3D({x, y, z}, {x, y, z}) {
+    
+// }
+
+
+
+class Square {
+    constructor({x, y}, l) {
+        this.x = x
+        this.y = y
+        this.l = l
+    }
+
+
+    getEdges() {
+        // returns the four edges of the square
+        return [
+            {x: this.x, y: this.y},
+            {x: -this.x, y: this.y},
+            {x: -this.x, y: -this.y},
+            {x: this.x, y: -this.y},
+
+        ]
+    }
+
+    diagonalEdge() {
+        return [
+            {x: this.x, y: this.y},
+            {x: this.x, y: -this.y}
+        ]
+    }
+
+    diagonalLength() {
+        return calcDist2D({ x1: this.x, y1: this.y }, { x2: this.x+this.l, y2: this.y+this.l })
+    }
+
+    area() {
+        return this.l * this.l
+    }
+
+    // fill() {
+        
+    // }
+}
+
+
+class Cube {
+    constructor({x, y, z}, l) {
+        if (!x || !y || !z || !l) {
+            console.log("Provide all the parameters")
+        }
+
+        this.x = x
+        this.y = y
+        this.z = z
+        this.l = l
+        this.face = new Square({x, y}, l)
+    }
+
+    getEdges() {
+        // console.log(this.l)
+        // const edges = []
+        const edges = [
+            {x: -this.x, y: this.y, z: this.z},
+            {x: this.x, y: this.y, z: this.z},
+            {x: this.x, y: -this.y, z: this.z},
+            {x: -this.x, y: -this.y, z: this.z},
+            
+            {x: -this.x, y: this.y, z: -this.z},
+            {x: this.x, y: this.y, z: -this.z},
+            {x: this.x, y: -this.y, z: -this.z},
+            {x: -this.x, y: -this.y, z: -this.z},
+
+        ]
+        // // console.log(edges)
+        
+        // const edges = []
+        // for (let edge of this.face.getEdges()) {
+        //     edges.push({x: edge.x, y: edge.y, z: this.z})
+
+        // }
+
+        // for (let edge of this.face.getEdges()) {
+        //     edges.push({x: edge.x, y: edge.y, z: -this.z})
+
+        // }
+
+        // console.log(edges)
+
+        return edges
+
+    }
+
+    fill(step = 0.05) {
+        const fillarPoints = []
+        const edges = this.getEdges()
+        const half = this.l
+
+        for (let u = -half; u <= half; u += step) {
+            for (let v = -half; v <= half; v += step) {
+                const p3d = {
+                    x: edges[0].x + u + half,
+                    y: edges[0].y + v - half,
+                    z: edges[0].z                    
+                }
+                
+                const side1 = rotate_x({x: p3d.x, y: p3d.y, z: p3d.z}, Math.PI/2)
+                const side2 = rotate_x({x: p3d.x, y: p3d.y, z: p3d.z}, -Math.PI/2)
+                fillarPoints.push(side1)
+                fillarPoints.push(side2)
+
+
+                for (let angle=0; angle < Math.PI*2; angle+=Math.PI/2) {
+                    const side = rotate_y(p3d, angle)
+                    
+                    fillarPoints.push(side)
+
+                }
+            }
+        }
+        return fillarPoints
+    }
+
+}
+
+// let test = new Cube({x: 10, y: 10, z: 10}, 5)
+// console.log(test.getEdges())
+
+// vs = [
+//     {x: 0.3, y: 0.3, z: 0.3}, // tl
+//     {x: -0.3, y: 0.3, z: 0.3}, // tr
+//     {x: -0.3, y: -0.3, z: 0.3}, // br
+//     {x: 0.3, y: -0.3, z: 0.3}, // bl
+
+//     {x: 0.3, y: 0.3, z: -0.3},
+//     {x: -0.3, y: 0.3, z: -0.3},
+//     {x: -0.3, y: -0.3, z: -0.3},
+//     {x: 0.3, y: -0.3, z: -0.3},
+
+// ]
+
+// const faces = [
+//     [0, 1, 2, 3],
+//     [4, 5, 6, 7],
+//     // [0, 4],
+//     // [1, 5],
+//     // [2, 6],
+//     // [3, 7],
+
+// ]
+
+// function drawFaces() {
+    
+// }
+
+
+function translateZ({x, y, z}, dz) {
+    return {
+        x, y, z: z + dz
+    }
+}
+
+function rotate_y({x, y, z}, angle) {
+    const cos = Math.cos(angle)
+    const sin = Math.sin(angle)
+
+    // Rotate around the viewer
+    return {
+        x: x * cos + z * sin,
+        y: y,
+        z: z * cos - x * sin,
+    }
+
+    // return {
+    //     x: x * cos - z * sin,
+    //     y: x * sin + z * cos,
+    //     z: z
+    // }
+
+
+}
+
+function rotate_x({x, y, z}, angle) {
+    const cos = Math.cos(angle)
+    const sin = Math.sin(angle)
+
+    return {
+        x: x,
+        y: y * cos - z * sin,
+        z: y * sin + z * cos
+    }
+}
+
+
+function rotate_z({x, y, z}, angle) {
+    const sin = Math.sin(angle)
+    const cos = Math.cos(angle)
+
+    return {
+        x: x * cos - y * sin,
+        y: x * sin + y * cos,
+        z: z
+    }
+
+}
+
+function screenPoint({x, y, z}) {
+    return screen(project({x, y, z}))
+}
+
+function plot3D({x, y, z}) {
+    point(screenPoint({x, y, z}))
+    return screen
+}
+
+const FPS = 60
+dz = 1
+theta = 0
+beta = 0
+
+const cube = new Cube({x: 0.4, y: 0.4, z: 0.4}, 0.4)
+let edges = cube.getEdges()
+
+function frame() {
+    dt = 1/FPS
+    // dz += 1 * dt
+    theta += Math.PI / 2 * dt
+    beta += Math.PI / 2 * dt
+    clear()
+
+
+    let fillarPoints = cube.fill(0.1, dz)
+    
+
+
+    for (let i in fillarPoints) {
+        // let points = screenPoint(translateZ(rotate_y(fillarPoints[i], theta), dz))
+        let points = screenPoint(translateZ(rotate_x(rotate_y(fillarPoints[i], theta), beta), dz))
+
+        point(points)
+        
+        // line(
+        //     points,
+        //     screenPoint(translateZ(rotate_y(edges[(i+1) % edges.length], theta), dz))
+        // )
+
+    }
+
+
+
+
+    // colorSide(
+    //     screenPoint(translateZ(rotate_y(edges[0], theta), dz)),
+    //     screenPoint(translateZ(rotate_y(edges[1], theta), dz)),
+    // )
+
+
+    // for (f of faces) {
+    //     for (let i=0; i < f.length; i++) {
+    //         const p1 = vs[f[i]]
+    //         const p2 = vs[f[(i+1) % f.length]]
+    //         // console.log(dz)
+    //         line(
+    //             // screen(project(translateZ(rotate_y(p1, theta), beta), dz)),
+    //             // screen(project(translateZ(rotate_y(p2, theta), beta), dz)),
+    //             screenPoint(translateZ(rotate_y(p1, theta), dz)),
+    //             screenPoint(translateZ(rotate_y(p2, theta), dz)),
+
+    //         )
+
+    //     }
+
+    // }
+
+    // setTimeout(frame, 1000/FPS)
+
+}
+
+setTimeout(frame, 1000/FPS)
+
+
